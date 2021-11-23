bcde4e740f7eeb77e595d7905d023e9517432e09
rafael.gracioli.glo <rafael.gracioli@globalhitss.com.br>
2021-11-23 18:24:28 -0300

    feat(logn): criação do componente, configuração da rota, html e scss

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
