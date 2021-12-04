57fe405ee8f834c54a1ab39074a196d092a037a6
Melfre Diego Rodrigues Silva <ctl_mrodrigues@uolinc.com>
2021-12-04 02:22:39 -0300

    feat: Add app-rounting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './pages/login/login/login.component';
 import { AuthService } from './services/auth/auth.service';
 import { SharedModule } from './shared/shared.module';
-import { LoginComponent } from './pages/login/login/login.component';
 
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AppRoutingModule
   ],
   declarations: [	
     AppComponent, HeaderComponent, NavComponent, LoginComponent
