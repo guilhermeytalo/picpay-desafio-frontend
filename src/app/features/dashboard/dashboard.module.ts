134ec58351ac34637da9599d84ea58d6806ad5ee
Ronaldo <ronaldoarg@gmail.com>
2022-01-06 19:32:51 -0300

    fix dashboard routing

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
-
+import { DashboardRoutingModule } from './dashboard.routes';
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule
   ]
 })
 export class DashboardModule { }
