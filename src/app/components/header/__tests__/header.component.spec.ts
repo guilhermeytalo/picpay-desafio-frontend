22c295de6e1aa11406ea42170af682d97902033a
Ronaldo <ronaldoarg@gmail.com>
2022-01-08 23:59:09 -0300

    include tests for header component

--- src/app/components/header/__tests__/header.component.spec.ts
+++ src/app/components/header/__tests__/header.component.spec.ts
@@ -1,12 +1,15 @@
 import { AuthModule } from '@/features/auth/auth.module';
+import { AuthService } from '@/features/auth/services/auth.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthServiceMock } from 'src/_mock/services/auth.service.mock';
 
 import { HeaderComponent } from '../header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let service: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,18 +17,26 @@ describe('HeaderComponent', () => {
         AuthModule,
         RouterTestingModule.withRoutes([])
       ],
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
+      providers: []
     })
       .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
+    service = TestBed.inject(AuthService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should logout', () => {
+    spyOn(service, 'logout').and.callFake(() => true);
+    component.logout();
+    expect(service.logout).toHaveBeenCalled();
   });
 });
