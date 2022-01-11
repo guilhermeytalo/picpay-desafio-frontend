8e0b38e60db30e80371f1cef0a7f79f4b023f9ce
Luan Souza <luan@stilingue.com.br>
2022-01-11 19:09:34 -0300

    fix(login): add email vcalidation to email field

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup} from '@angular/forms';
 import { LoginService } from '../../services/login/login.service';
 
 @Component({
@@ -13,12 +13,16 @@ export class LoginComponent implements OnInit {
   public password = '';
   public shouldShowPassword = false;
 
-  emailControl = new FormControl('', [Validators.required]);
-  passwordControl = new FormControl('', [Validators.required]);
+  public loginForm;
   emailPattern = '[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}';
 
   ngOnInit(): void {
     this.loginService.checkLogin();
+
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+    });
   }
 
   togglePassword(): void {
