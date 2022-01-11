53a3fef78c205bf2c4d14610b04c502657ece1db
Luan Souza <luan@stilingue.com.br>
2022-01-11 10:10:20 -0300

    test: correct .spec files issues

--- src/app/services/feedback/feedback.service.spec.ts
+++ src/app/services/feedback/feedback.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FeedbackService } from './feedback.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FeedbackService', () => {
   let service: FeedbackService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MatSnackBarModule],
+      imports: [RouterTestingModule, MatSnackBarModule, BrowserAnimationsModule],
+    });
     service = TestBed.inject(FeedbackService);
   });
 
