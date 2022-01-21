c4720a7a96767607a1d83cfa0de8383ca4ee57c4
dxwebster <adrianalimafm@gmail.com>
2022-01-21 13:51:46 -0300

    build: add pages and home modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+// Modules
+import { PagesModule } from "./pages/pages.module";
+
+// Components
+
 @NgModule({
-  declarations: [	
-    AppComponent,
-   ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
