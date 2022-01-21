4ae3d094c6da4b8c6aa502ee4a7fe97f6f05a513
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-21 17:44:35 -0300

    [DESAFIO-PICPAY] chore: add translocoModule AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
 @NgModule({
   declarations: [	
     AppComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslocoRootModule
   ],
   providers: [],
   bootstrap: [AppComponent]
