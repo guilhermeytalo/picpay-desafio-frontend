07d4c2304abb5f4d4cf89027ab0dff8b9bbd376b
Jhonata Ewerton Marcos <jhonata.as@hotmail.com>
2022-01-21 18:55:11 -0300

    includes some modules like table.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { AppRoutingModule } from './app.routing';
+import { AppRoutingModule } from "./app.routing";
 import { MaterialModule } from "./material.module";
-import { AuthService } from "./pages/auth/shared/auth.service";
 import { AuthComponent } from "./pages/auth/auth.component";
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -11,11 +10,21 @@ import { HttpClientModule } from "@angular/common/http";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { LoginComponent } from './components/form/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from "./components/form/login/login.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { TableComponent } from "./components/table/table.component";
+import { MatPaginatorIntl } from "@angular/material/paginator";
+import { MatPaginatorIntlPtBr } from "./components/table/paginator/paginator-ptbr";
 @NgModule({
-  declarations: [AppComponent, AuthComponent, LoginComponent, HomeComponent, NavBarComponent],
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    HomeComponent,
+    NavBarComponent,
+    TableComponent,
+  ],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -24,9 +33,14 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     HttpClientModule,
     FlexLayoutModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MatPaginatorIntl,
+      useClass: MatPaginatorIntlPtBr,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
