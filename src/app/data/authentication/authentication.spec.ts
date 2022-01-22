f3f60b275bdff145520261f3bd79c9cbb508b790
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-22 17:57:02 -0300

    [DESAFIO-PICPAY] refactor: add util faker tests

--- src/app/data/authentication/authentication.spec.ts
+++ src/app/data/authentication/authentication.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthenticationParams } from '@domain/usecases/authentication';
-import { IHttpClient } from '../protocols/http-post-client';
+import { IHttpClient } from '../protocols/http-client';
 import { AuthenticationService } from './authentication.service';
-
+import faker from 'faker';
 let authenticationService: AuthenticationService;
 let httpService: jasmine.SpyObj<IHttpClient>;
 
@@ -24,8 +24,8 @@ describe('AuthenticationService', () => {
   });
   it('Should request auth with body', () => {
     const body: AuthenticationParams = {
-      email: 'mock@email.com',
-      password: 'mock@password'
+      email: faker.internet.email(),
+      password: faker.internet.password()
     };
     authenticationService.auth(body);
     expect(httpService.post).toHaveBeenCalled();
