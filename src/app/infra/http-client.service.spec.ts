264066b30a278cde4dfc16eda387f5fa2e26aaf0
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-22 18:06:45 -0300

    [DESAFIO-PICPAY] test: add more test cases HttpClientService

--- src/app/infra/http-client.service.spec.ts
+++ src/app/infra/http-client.service.spec.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientService } from './http-client.service';
-
+import faker from 'faker';
+import { of } from 'rxjs';
 let service: HttpClientService;
 let httpClientSpy: jasmine.SpyObj<HttpClient>;
 describe('HttpClientService', () => {
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('IHttpClient', ['post']);
+    httpClientSpy = jasmine.createSpyObj('IHttpClient', ['get']);
     TestBed.configureTestingModule({
       providers: [
         HttpClientService,
@@ -18,5 +19,12 @@ describe('HttpClientService', () => {
   });
   it('should init httpClientService', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should request with  URL GET', () => {
+    httpClientSpy.get.and.returnValue(of(faker.internet.url(), {}));
+    const url = faker.internet.url();
+    service.get(url, {});
+    expect(httpClientSpy.get).toHaveBeenCalledWith(url, {});
   });
 });
