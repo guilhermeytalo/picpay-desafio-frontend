a04ee1e564c4cc98ed8da8ee7e577a76d73c9335
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-22 18:09:51 -0300

    [DESAFIO-PICPAY] refactor: rename Class Http on DataModule

--- src/app/data/data.module.ts
+++ src/app/data/data.module.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { HttpClientService } from '@app/infra/http-client.service';
 import { AuthenticationService } from './authentication/authentication.service';
-import { IHttpClient } from './protocols/http-post-client';
+import { IHttpClient } from './protocols/http-client';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
