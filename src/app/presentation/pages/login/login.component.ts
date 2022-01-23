1fa1598d57db4ffbb4b39d8d376a5ed535c1210a
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-23 19:48:19 -0300

    [DESAFIO-PICPAY] feat: create component to supply the tests

--- src/app/presentation/pages/login/login.component.ts
+++ src/app/presentation/pages/login/login.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, SkipSelf } from '@angular/core';
+import { IAuthentication } from '@app/domain/usecases/authentication';
+import { ISnackBar } from '@app/shared/class/isnackbar';
+import { CredentialModel } from '@app/shared/forms-model/credential.model';
+import { FormHelper } from '@shared/helpers/form.helper';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent extends FormHelper {
+  private _loading = false;
+  constructor(
+    @SkipSelf() private readonly authenticationService: IAuthentication,
+    @SkipSelf() private readonly snackBar: ISnackBar
+  ) {
+    super(new CredentialModel());
+  }
 
-  ngOnInit() {}
+  auth() {
+    this._loading = true;
+    this.authenticationService.auth(this.form.value).subscribe((res) => {
+      this._loading = false;
+      if (res) {
+        console.log(res);
+      } else {
+        this.snackBar.openSnackBar('message.error', 'error');
+      }
+    });
+  }
+  get loading(): boolean {
+    return this._loading;
+  }
 }
