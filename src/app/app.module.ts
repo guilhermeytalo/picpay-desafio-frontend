a3b24cd3fd5c4ff247c22e6791fbef02e4b0e6a6
dxwebster <adrianalimafm@gmail.com>
2022-01-23 20:05:50 -0300

    feature: includes modal animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser"
+import { NgModule } from "@angular/core"
+import { HttpClientModule } from "@angular/common/http"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
 
-import { PagesModule } from "./pages/pages.module";
+import { PagesModule } from "./pages/pages.module"
+
+/* Importando a configuração de algumas linguagens */
+import { registerLocaleData } from "@angular/common"
+import localePT from "@angular/common/locales/pt"
+
+registerLocaleData(localePT)
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PagesModule, HttpClientModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, PagesModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -2,19 +2,26 @@ import { Component, OnInit } from "@angular/core"
 import { Router } from "@angular/router"
 import { PaymentsApiService } from "../../service/payments-api.service"
 import { Payment } from "../../service/payment"
+import { trigger, style, animate, transition } from "@angular/animations"
 
 @Component({
   selector: "modal",
   templateUrl: "./modal.component.html",
   styleUrls: ["./modal.component.scss"],
+  animations: [
+    trigger("overlay", [
+      transition(":enter", [style({ opacity: 0 }), animate("250ms", style({ opacity: 0.5 }))]),
+      transition(":leave", [animate("500ms", style({ opacity: 0 }))]),
+    ]),
+
+    trigger("modal", [
+      transition(":enter", [style({ top: -999 }), animate("500ms", style({ top: "50%" }))]),
+      transition(":leave", [animate("250ms", style({ top: -999 }))]),
+    ]),
+  ],
 })
 export class ModalComponent implements OnInit {
   public mostrar: boolean = false
-
-  payment: Payment = new Payment()
-  title: string
-
-  constructor(private router: Router, private paymentsApiService: PaymentsApiService) {}
 
   ngOnInit() {}
 
