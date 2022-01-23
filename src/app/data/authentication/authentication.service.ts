edf6566be844dedcb3d4ce37e756cabbd465e7d4
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-23 19:37:03 -0300

    [DESAFIO-PICPAY] refactor:  add pipe and map AuthenticationService

--- src/app/data/authentication/authentication.service.ts
+++ src/app/data/authentication/authentication.service.ts
@@ -5,13 +5,23 @@ import {
   AuthenticationParams,
   IAuthentication
 } from '@domain/usecases/authentication';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { IHttpClient } from '../protocols/http-client';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthenticationService implements IAuthentication {
   constructor(private readonly http: IHttpClient) {}
-  auth(params: AuthenticationParams): Observable<AccountModel[]> {
-    return this.http.get(Routes.getUser, params);
+  auth(params: AuthenticationParams): Observable<AccountModel> {
+    return this.http
+      .get<AccountModel[], AuthenticationParams>(Routes.getUser, params)
+      .pipe(
+        map((users) =>
+          users.find(
+            (user) =>
+              user.email === params.email && user.password === params.password
+          )
+        )
+      );
   }
 }
