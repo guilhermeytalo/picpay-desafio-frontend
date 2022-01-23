5460f78f7ee2cd5003ce78011589144be550aeb5
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-23 19:33:46 -0300

    [DESAFIO-PICPAY] feat: add dataModule and infraModule on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { TranslocoRootModule } from './transloco-root.module';
 import { RouterModule } from '@angular/router';
 import { appRoutingModule } from './app.routing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataModule } from './data/data.module';
+import { InfraModule } from './infra/infra.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -15,7 +17,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     TranslocoRootModule,
     RouterModule,
     appRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DataModule,
+    InfraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
