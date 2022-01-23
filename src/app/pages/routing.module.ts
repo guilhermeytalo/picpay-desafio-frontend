2b10af92e6888f813f7ab9769c2e2c17bd80a32c
dxwebster <adrianalimafm@gmail.com>
2022-01-22 21:43:28 -0300

    chore: fix css styles

--- src/app/pages/routing.module.ts
+++ src/app/pages/routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "../shared/auth/auth.guard";
+
+// Components
 import { LoginFormComponent } from "../shared/login-form/login-form.component";
 import { PaymentsListComponent } from "../shared/payments-list/payments-list.component";
 
+// Pages
 import { Dashboard } from "./dashboard/dashboard.component";
 import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   {
-    path: "dashboard",
+    path: "",
     component: Dashboard,
     children: [{ path: "", component: PaymentsListComponent }],
     canActivate: [AuthGuard],
--- src/app/shared/auth/auth.guard.ts
+++ src/app/shared/auth/auth.guard.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const token = window.localStorage.getItem("token");
+
+    if (token) {
+      return true;
+    } else {
+      this.router.navigate(["login"]);
+      return false;
+    }
   }
-  
 }
