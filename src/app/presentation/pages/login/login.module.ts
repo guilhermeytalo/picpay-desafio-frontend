e32e6c8ebd41d917b4e6c5cccc781446569ce95e
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-23 02:03:42 -0300

    [DESAFIO-PICPAY] feat: create login layout

--- src/app/presentation/pages/login/login.module.ts
+++ src/app/presentation/pages/login/login.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import AngularMaterialModule from '@app/presentation/components/angular-material/angular-material.module';
+import { TranslocoModule } from '@ngneat/transloco';
 import { LoginComponent } from './login.component';
 import { loginRoutingModule } from './login.routing';
 
 @NgModule({
-  imports: [loginRoutingModule],
+  imports: [TranslocoModule, AngularMaterialModule, loginRoutingModule],
   exports: [],
   declarations: [LoginComponent],
   providers: []
