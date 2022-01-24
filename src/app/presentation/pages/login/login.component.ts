b5ee1aa7c7ed9c2d636300b9a0d950ea294f1619
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-24 12:12:56 -0300

    [DESAFIO-PICPAY] feat: integration front x back

--- src/app/presentation/pages/login/login.component.ts
+++ src/app/presentation/pages/login/login.component.ts
@@ -1,8 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, SkipSelf } from '@angular/core';
+import { AccountModel } from '@app/domain/models/account.model';
 import { IAuthentication } from '@app/domain/usecases/authentication';
 import { CredentialModel } from '@app/shared/forms-model/credential.model';
+import ErrorResponseHelper from '@app/shared/helpers/error-response.helper';
 import { ISnackBar } from '@app/shared/interfaces/isnackbar';
 import { FormHelper } from '@shared/helpers/form.helper';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -20,14 +24,22 @@ export class LoginComponent extends FormHelper {
 
   auth() {
     this._loading = true;
-    this.authenticationService.auth(this.form.value).subscribe((res) => {
-      this._loading = false;
-      if (res) {
-        console.log(res);
-      } else {
-        this.snackBar.openSnackBar('message.error', 'error');
-      }
-    });
+    this.authenticationService
+      .auth(this.form.value)
+      .pipe(
+        finalize(() => {
+          this._loading = false;
+        })
+      )
+      .subscribe({
+        next: (user: AccountModel) => {
+          console.log(user);
+        },
+        error: (helpError: ErrorResponseHelper) => {
+          const { message } = helpError.error.error;
+          this.snackBar.openSnackBar(message, 'error');
+        }
+      });
   }
   get loading(): boolean {
     return this._loading;
