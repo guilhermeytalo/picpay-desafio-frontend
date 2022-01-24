71101a58c0ae46cae707c88082285677e72ff967
Jhonata Ewerton Marcos <jhonata.as@hotmail.com>
2022-01-24 14:15:29 -0300

    import interceptor and new modules in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { AppRoutingModule } from "./app.routing";
 import { MaterialModule } from "./material.module";
 import { AuthComponent } from "./pages/auth/auth.component";
 import { BrowserModule } from "@angular/platform-browser";
-import {  NgModule } from "@angular/core";
+import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { LoginComponent } from "./components/form/login/login.component";
 import { HomeComponent } from "./pages/home/home.component";
@@ -17,7 +17,10 @@ import { TableComponent } from "./components/table/table.component";
 import { MatPaginatorIntl } from "@angular/material/paginator";
 import { MatPaginatorIntlPtBr } from "./share/utils/paginator/paginator-ptbr";
 import { DialogComponent } from "./components/dialog-confirm/dialog.component";
-import { CreateComponent } from './components/form/create/create.component';
+import { CreateComponent } from "./components/form/create/create.component";
+import { ProfileComponent } from "./pages/profile/profile.component";
+import { SpinnerComponent } from "./components/spinner/spinner.component";
+import { HttpRequestInterceptor } from "./core/interceptors/http-request.interceptor";
 
 @NgModule({
   declarations: [
@@ -29,6 +32,8 @@ import { CreateComponent } from './components/form/create/create.component';
     TableComponent,
     DialogComponent,
     CreateComponent,
+    ProfileComponent,
+    SpinnerComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -39,11 +44,17 @@ import { CreateComponent } from './components/form/create/create.component';
     FlexLayoutModule,
     ReactiveFormsModule,
     RouterModule,
+    FormsModule,
   ],
   providers: [
     {
       provide: MatPaginatorIntl,
       useClass: MatPaginatorIntlPtBr,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true,
     },
   ],
   entryComponents: [DialogComponent],
