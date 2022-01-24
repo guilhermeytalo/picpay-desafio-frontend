993231b915a9ec236ca7969a45e849170b0ef713
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-24 12:12:18 -0300

    [DESAFIO-PICPAY] refactor: add new Error if user not found

--- src/app/data/authentication/authentication.service.ts
+++ src/app/data/authentication/authentication.service.ts
@@ -7,7 +7,9 @@ import {
 } from '@domain/usecases/authentication';
 import { Observable, of, throwError } from 'rxjs';
 import { IHttpClient } from '../protocols/http-client';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import ErrorResponseHelper from '@app/shared/helpers/error-response.helper';
+import { ErrorEnum } from '@app/shared/helpers/errors-key.enum';
 
 @Injectable()
 export class AuthenticationService implements IAuthentication {
@@ -16,12 +18,17 @@ export class AuthenticationService implements IAuthentication {
     return this.http
       .get<AccountModel[], AuthenticationParams>(Routes.getUser, params)
       .pipe(
-        map((users) =>
-          users.find(
+        map((users) => {
+          const findUser = users.find(
             (user) =>
               user.email === params.email && user.password === params.password
-          )
-        )
+          );
+          if (findUser) {
+            return findUser;
+          } else {
+            throw new ErrorResponseHelper(ErrorEnum.userNotFound, 404);
+          }
+        })
       );
   }
 }
