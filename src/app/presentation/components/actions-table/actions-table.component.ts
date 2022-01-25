3247f11f1cd294c72ea2c0728c3d2f5d2c32edca
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-25 09:01:00 -0300

    [DESAFIO-PICPAY] feat: create componetn actionTable

---
+++ src/app/presentation/components/actions-table/actions-table.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actions-table',
+  templateUrl: 'actions-table.component.html'
+})
+export class ActionsTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/presentation/components/components.module.ts
+++ src/app/presentation/components/components.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ISnackBar } from '@shared/interfaces/isnackbar';
+import { ActionsTableComponent } from './actions-table/actions-table.component';
 import AngularMaterialModule from './angular-material/angular-material.module';
 import { SortTableComponent } from './sort-table/sort-table.component';
 import { SnackBarService } from './toast/snackbar';
 
 @NgModule({
   imports: [AngularMaterialModule],
-  exports: [AngularMaterialModule, SortTableComponent],
-  declarations: [SortTableComponent],
+  exports: [AngularMaterialModule, SortTableComponent, ActionsTableComponent],
+  declarations: [SortTableComponent, ActionsTableComponent],
   providers: [
     {
       provide: ISnackBar,
