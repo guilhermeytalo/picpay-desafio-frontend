2d5c188e682139d71f782ffcbbbcdddc0a69fada
Max <maximiliano@omotor.com.br>
2022-01-24 23:31:46 -0300

    feat: criados os componentes de cada rota

---
+++ src/app/routes/listar-pagamentos/listar-pagamentos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pagamentos',
+  templateUrl: './listar-pagamentos.component.html',
+  styleUrls: ['./listar-pagamentos.component.scss']
+})
+export class ListarPagamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
