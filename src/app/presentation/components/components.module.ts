3796f8ab4e16c50268e45f475e06d4a312e9faad
Waldir Feitoza de Azevedo Neto <waldir.azevedo@venturus.org.br>
2022-01-25 10:21:07 -0300

    [DESAFIO-PICPAY] refactor: layout payment

--- src/app/presentation/components/components.module.ts
+++ src/app/presentation/components/components.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TranslocoModule } from '@ngneat/transloco';
 import { ISnackBar } from '@shared/interfaces/isnackbar';
 import { ActionsTableComponent } from './actions-table/actions-table.component';
 import AngularMaterialModule from './angular-material/angular-material.module';
+import { FilterTableComponent } from './filter-table/filter-table.component';
+import { PaginationComponent } from './pagination/pagination.component';
 import { SortTableComponent } from './sort-table/sort-table.component';
 import { SnackBarService } from './toast/snackbar';
 
 @NgModule({
-  imports: [AngularMaterialModule],
-  exports: [AngularMaterialModule, SortTableComponent, ActionsTableComponent],
-  declarations: [SortTableComponent, ActionsTableComponent],
+  imports: [TranslocoModule, AngularMaterialModule],
+  exports: [
+    FormsModule,
+    AngularMaterialModule,
+    SortTableComponent,
+    ActionsTableComponent,
+    FilterTableComponent,
+    PaginationComponent
+  ],
+  declarations: [
+    SortTableComponent,
+    ActionsTableComponent,
+    FilterTableComponent,
+    PaginationComponent
+  ],
   providers: [
     {
       provide: ISnackBar,
---
+++ src/app/presentation/components/filter-table/filter-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-table',
+  templateUrl: 'filter-table.component.html',
+  styleUrls: ['filter-table.component.scss']
+})
+export class FilterTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
