de57bd7f77262113dd74cae10d35e01a3bc0fac9
Max <maximiliano@omotor.com.br>
2022-01-25 22:18:13 -0300

    feat: criados os modulos header e sidebar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
